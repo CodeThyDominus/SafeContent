5ece0b20245b7c0c30b3cd605ecd3bf919b3e456
CodeThyDominus <thydominus@gmail.com>
2023-10-12 15:34:12 +0530

    creating scrapy spider parsing data using bs4 using selenium webdriver for web interaction

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import scrapy
+from bs4 import BeautifulSoup
+from selenium import webdriver
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# scrapy spider
+class InappropriateWordsSpider(scrapy.Spider):
+    name = 'inappropriate_words'
+    start_urls = ['https://www.noswearing.com/dictionary']
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    def parse(self, response):
+        soup = BeautifulSoup(response.body, 'html.parser')
+        words = [word.strip() for word in soup.get_text().split('\n')]
+        yield {'words': words}
+
+    # selenium for web interaction
+    def scrape_web_content(url):
+        # initializing Selenium webdriver
+        driver = webdriver.Chrome(executable_path='webdriver path')
+        driver.get(url)
+
+        # extracting the webpage's HTML source
+        html_source = driver.page_source
+        driver.quit()  # Close the WebDriver
+
+        return html_source
